8c8de8fa2ce6bb30d52b49be0b836d18ac83d2d8
malawad <50057882+malawadd@users.noreply.github.com>
2023-09-29 03:44:41 +0300

    🎆 added wagmi config 🎆

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Main } from './pages/main';
+
+import { configureChains, createClient,  WagmiConfig } from 'wagmi';
+import { localhost } from 'wagmi/chains';
+import { rskTestnet } from './utils/rskTestnet';
+import { bitfinityTestnet } from './utils/bitfinityTestnet';
+
+
+import { publicProvider } from 'wagmi/providers/public';
+
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [ rskTestnet, bitfinityTestnet, localhost],
+  [publicProvider()]
+);
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({ chains, options: {
+    qrcode: true,
+    }}),
+  ],
+  provider,
+  webSocketProvider,
+});
+
+window.Buffer = window.Buffer || require('buffer').Buffer;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <WagmiConfig client={client}>
+      <Main />
+    </WagmiConfig>
+    </>
   );
 }
 
