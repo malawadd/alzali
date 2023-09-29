1ec7dfd6a67760f0ef5529f04042215512a47fc7
malawad <50057882+malawadd@users.noreply.github.com>
2023-09-29 04:51:29 +0300

    🧰 header done 🧰

--- webapp/src/pages/main.tsx
+++ webapp/src/pages/main.tsx
@@ -1,6 +1,7 @@
-import logo from './../logo.svg';
+import Logo from '../assets/logo.png';
 import { Connect } from '../components/connect';
 import { Network } from '../components/network';
+
 
 import './main.css';
 
@@ -10,6 +11,16 @@ export function Main() {
             <section className="layout">
                 <div className="content">
                     <div className="header-h">
+                        <div className="header-item">
+                            <img className="logo logo-bg" src={Logo} alt="Xcrypt" />
+                            <img
+                            className="logo logo-default"
+                            src={Logo}
+                            alt="Xcrypt"
+                            onClick={() => (document.location.href = '/')}
+                            />
+                        </div>
+                        
                         <div className="header-item">
                             <Connect />
                             <Network />
